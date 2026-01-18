e1e4e09be9e7e60cdc3af34d85784ed6b6366280
Alan Lima <alima@outlook.com>
2026-01-19 07:49:32 +1000

    rename folder for upbank-ui

--- apps/upbank-ui-simple/src/auth/config.ts
+++
@@ -1,9 +0,0 @@
-import type { EnvConfig } from '../config/envConfig'
-import { loadEnvConfig } from '../config/envConfig'
-import type { RuntimeConfig } from '../config/runtimeConfig'
-import { getRuntimeConfig, shouldUseRuntimeConfig } from '../config/runtimeConfig'
-
-export type AuthConfig = EnvConfig | RuntimeConfig
-
-export const getAuthConfig = (): AuthConfig =>
-  shouldUseRuntimeConfig() ? getRuntimeConfig() : loadEnvConfig()
