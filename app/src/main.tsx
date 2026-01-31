816e78f34801e0539756a643ce8255da4c2fa059
Alan Lima <alima@outlook.com>
2026-02-01 08:19:19 +1000

    add blue theme

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import RuntimeConfigGate from './components/RuntimeConfigGate.tsx'
+import { applyTheme } from './config/theme.ts'
+
+applyTheme()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
